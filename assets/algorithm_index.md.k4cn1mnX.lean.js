7eb1bdcd1a756d88d778a9d87f464e02f4c23f45
wyx <3062871098@qq.com>
2024-02-23 00:39:17 +0800

    wyx

--- assets/algorithm_index.md.k4cn1mnX.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as t,c as n,o as r,m as e,a}from"./chunks/framework.CsYP9yp8.js";const k=JSON.parse('{"title":"数据结构与算法的学习","description":"","frontmatter":{},"headers":[],"relativePath":"algorithm/index.md","filePath":"algorithm/index.md"}'),o={name:"algorithm/index.md"},s=e("h1",{id:"数据结构与算法的学习",tabindex:"-1"},[a("数据结构与算法的学习 "),e("a",{class:"header-anchor",href:"#数据结构与算法的学习","aria-label":'Permalink to "数据结构与算法的学习"'},"​")],-1),i=e("h2",{id:"常见的数据结构",tabindex:"-1"},[a("常见的数据结构 "),e("a",{class:"header-anchor",href:"#常见的数据结构","aria-label":'Permalink to "常见的数据结构"'},"​")],-1),d=e("pre",null,[e("code",null,`数据(Array)
-栈(Stack)
-链表(Linked List)
-图(Graph)
-散列表(Hash)
-队列(Queue)
-堆(Heap)
-树(Tree)
-`)],-1),c=[s,i,d];function l(h,_,m,p,f,x){return r(),n("div",null,c)}const b=t(o,[["render",l]]);export{k as __pageData,b as default};
